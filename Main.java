b28106189ca75cf41b2c55bee04d879e918039f0
Sreecharanya Srinivasan <67717792+sreecharanya@users.noreply.github.com>
2021-10-25 22:11:56 +0000

    palindrome check

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    String str="Sree";
+    String str="madam";
 String reverse="";
 System.out.println("Original string "+str);
 for(int i=str.length()-1;i>=0;i--){
@@ -9,5 +9,13 @@ reverse=reverse+str.charAt(i);
 }
 System.out.println("Reversed string "+reverse);
 
+if(str.equals(reverse)){
+  System.out.println( "Palindrome");
+}
+  else{
+    System.out.println("Not palindrome");
   }
+}
+
+  
 }
