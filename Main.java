d97a0991f2a4c206842a7e6cca1b43dc8ec310df
sreecharanya <67717792+sreecharanya@users.noreply.github.com>
2021-10-25 13:07:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    String str="Sree";
+String reverse="";
+System.out.println(str);
+for(int i=str.length()-1;i>=0;i--){
+reverse=reverse+str.charAt(i);
+}
+System.out.println(reverse);
+
+  }
+}
